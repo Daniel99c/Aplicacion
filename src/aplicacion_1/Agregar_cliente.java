dcbe614dd6ecfdd36c5009e8f2567e04e86ea19b
LAPTOP-DQS18T03\Daniel Caceres <dcacerest6@gmail.com>
2022-09-02 19:55:50 -0500

    Aplicacion_Agregar numeros

--- src/aplicacion_1/Agregar_cliente.java
+++ src/aplicacion_1/Agregar_cliente.java
@@ -33,6 +33,7 @@ public class Agregar_cliente extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         numero = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         Resultado = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
@@ -49,24 +50,33 @@ public class Agregar_cliente extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion_1/2276042_document_extension_format_java_paper_icon (1).png"))); // NOI18N
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(34, 34, 34)
-                .addComponent(numero, javax.swing.GroupLayout.PREFERRED_SIZE, 529, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(37, Short.MAX_VALUE))
+                .addComponent(numero, javax.swing.GroupLayout.PREFERRED_SIZE, 377, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(182, 182, 182))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(48, 48, 48)
-                .addComponent(numero, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(61, Short.MAX_VALUE))
+                .addGap(77, 77, 77)
+                .addComponent(numero, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 600, 150));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 620, 180));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -89,8 +99,8 @@ public class Agregar_cliente extends javax.swing.JFrame {
 
         jPanel3.setLayout(new java.awt.GridLayout(1, 0));
 
-        jButton1.setBackground(new java.awt.Color(24, 44, 97));
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setBackground(new java.awt.Color(106, 176, 76));
+        jButton1.setForeground(new java.awt.Color(0, 0, 0));
         jButton1.setText("Agregar");
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -107,7 +117,7 @@ public class Agregar_cliente extends javax.swing.JFrame {
         });
         jPanel3.add(jButton1);
 
-        jButton2.setBackground(new java.awt.Color(88, 177, 159));
+        jButton2.setBackground(new java.awt.Color(106, 176, 76));
         jButton2.setForeground(new java.awt.Color(0, 0, 0));
         jButton2.setText("Mostrar");
         jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -125,7 +135,7 @@ public class Agregar_cliente extends javax.swing.JFrame {
         });
         jPanel3.add(jButton2);
 
-        jButton3.setBackground(new java.awt.Color(249, 127, 81));
+        jButton3.setBackground(new java.awt.Color(106, 176, 76));
         jButton3.setForeground(new java.awt.Color(0, 0, 0));
         jButton3.setText("Ordenar");
         jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -207,39 +217,40 @@ public static boolean validarNumeros(String datos)
 
     private void jButton1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseEntered
         // TODO add your handling code here:
-        jButton1.setBackground(new java.awt.Color(59, 59, 152));
+        jButton1.setBackground(new java.awt.Color(186, 220, 88));
         jButton1.setForeground(new java.awt.Color(0, 0, 0));
         
     }//GEN-LAST:event_jButton1MouseEntered
 
     private void jButton1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseExited
-        jButton1.setBackground(new java.awt.Color(24, 44, 97));
-        jButton1.setForeground(new java.awt.Color(0, 0, 0));
-        
+       
+      jButton1.setBackground(new java.awt.Color(106, 176, 76));
+      jButton1.setForeground(new java.awt.Color(0, 0, 0));
+    
     }//GEN-LAST:event_jButton1MouseExited
 
     private void jButton2MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseEntered
         // TODO add your handling code here:
-        jButton2.setBackground(new java.awt.Color(85, 230, 193));
+        jButton2.setBackground(new java.awt.Color(186, 220, 88));
         jButton2.setForeground(new java.awt.Color(0, 0, 0));
     }//GEN-LAST:event_jButton2MouseEntered
 
     private void jButton2MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseExited
         // TODO add your handling code here:
-        jButton2.setBackground(new java.awt.Color(88, 177, 159));
+        jButton2.setBackground(new java.awt.Color(106, 176, 76));
         jButton2.setForeground(new java.awt.Color(0, 0, 0));
     }//GEN-LAST:event_jButton2MouseExited
 
     private void jButton3MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseEntered
         // TODO add your handling code here:
-         jButton3.setBackground(new java.awt.Color(254, 164, 127));
+        jButton3.setBackground(new java.awt.Color (186, 220, 88));
         jButton3.setForeground(new java.awt.Color(0, 0, 0));
         
     }//GEN-LAST:event_jButton3MouseEntered
 
     private void jButton3MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseExited
         // TODO add your handling code here:
-        jButton3.setBackground(new java.awt.Color(249, 127, 81));
+        jButton3.setBackground(new java.awt.Color(106, 176, 76));
         jButton3.setForeground(new java.awt.Color(0, 0, 0));
         
     }//GEN-LAST:event_jButton3MouseExited
@@ -284,6 +295,7 @@ public static boolean validarNumeros(String datos)
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
